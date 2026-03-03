ded7dc71f143c1439b23e785b3e025b1341a4690
Santosh <santosh@Santoshs-MacBook-Pro.local>
2026-03-03 16:20:27 +0530

    chore: remove ENTERPRISE edition constraint from Cloud SQL instance
    Agent-Id: agent-306ffe22-2b70-4ef5-8251-1a824c931d66

--- infra/terraform/modules/cloudsql/main.tf
+++ infra/terraform/modules/cloudsql/main.tf
@@ -13,7 +13,6 @@ resource "google_sql_database_instance" "main" {
   name             = "${var.name_prefix}-postgres"
   database_version = "POSTGRES_16"
   region           = var.gcp_region
-  edition          = "ENTERPRISE"
 
   deletion_protection = true
 
